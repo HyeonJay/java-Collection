f83b26507c697316518e0064b431f900e3a8f79d
onsoftel <onsoftel@hanmail.net>
2015-07-03 14:53:47 +0900

    2015. 7. 3 javaCollection

---
+++ javaCollection/src/thread/RunnableImpl.java
@@ -0,0 +1,14 @@
+package thread;
+
+public class RunnableImpl implements Runnable {
+
+	@Override
+	public void run( ) {
+		for ( int i = 0; i < 5; i++ ) {
+			// Thread는 쓰레드를 구현한 클래스다. 
+			System.out.println( Thread.currentThread( ).getName( ) );
+		}
+
+	}
+
+}
